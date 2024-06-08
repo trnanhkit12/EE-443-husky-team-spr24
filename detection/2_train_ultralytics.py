ee276aa8949f921e789cf4555cb1e038bfdc7502
trnanhkit12 <trnanhkit12@gmail.com>
2024-06-08 02:26:06 +0000

    modified model being used to yolo8vx

--- detection/2_train_ultralytics.py
+++ detection/2_train_ultralytics.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 # Load a model
-model = YOLO('yolov8n.yaml')  # build a new model from YAML
+model = YOLO('yolov8x.yaml')  # build a new model from YAML
 
 # Train the model
 results = model.train(data='/media/cycyang/sda1/EE443_final/detection/ee443.yaml', epochs=100, imgsz=640)
